8bcb1fa2e9689b2803b517532b98007b537e0399
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-24 18:48:22 -0400

    added 4b

--- ArrayOps.java
+++ ArrayOps.java
@@ -9,4 +9,11 @@ public class ArrayOps {
     }
     return sum;
   }
+  public static int largest(int[] arr) {
+    int max = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] > max) max = arr[i];
+    }
+    return max;
+  }
 }
