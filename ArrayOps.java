ba3c47f02457eaca316a04f18c6e61f85988b21f
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-24 18:44:21 -0400

    Added to 4a

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,8 +1,11 @@
 public class ArrayOps {
   public static int sum(int[] arr) {
     int sum = 0;
-    for (int i = 0; i < arr.length; i++) {
+    if (arr.length == 0) return 0;
+    else {
+      for (int i = 0; i < arr.length; i++) {
       sum = sum + arr[i];
+      }
     }
     return sum;
   }
