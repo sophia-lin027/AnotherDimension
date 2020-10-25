c954798668ffd1aa4fa138ccd26a265ae5f5950a
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-25 19:03:16 -0400

    added 5c

--- Tester.java
+++ Tester.java
@@ -11,5 +11,6 @@ public class Tester {
     System.out.println(ArrayOps.sum(A));
     System.out.println(Arrays.toString(ArrayOps.sumCols(A)));
     System.out.println(ArrayOps.isRowMagic(A));
+    System.out.println(ArrayOps.isColMagic(A));
   }
 }
