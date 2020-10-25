dec961ac75841900dd5daa15083c36badc3b224b
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-25 10:32:01 -0400

    added 5e

--- Tester.java
+++ Tester.java
@@ -9,5 +9,6 @@ public class Tester {
     System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
     System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
     System.out.println(ArrayOps.sum(A));
+    System.out.println(Arrays.toString(ArrayOps.sumCols(A)));
   }
 }
