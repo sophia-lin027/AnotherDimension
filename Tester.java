cf146b6975daa2098b70a024ebefb9d72bce41d1
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-24 22:21:45 -0400

    added 4d

--- Tester.java
+++ Tester.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class Tester {
   public static void main(String[] args){
-    int[] array = {};
-    int[] array1 = {3, 4, 5};
+    int[] B = {1, 3, 5};
     int[][] A = {{1, 0, 12, -1}, {7, -2, 2, 1}, {-5, -2, 2, -9}};
-    System.out.println(ArrayOps.sum(array));
-    System.out.println(ArrayOps.largest(array1));
+    System.out.println(ArrayOps.sum(B));
+    System.out.println(ArrayOps.largest(B));
     System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
+    System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
   }
 }
