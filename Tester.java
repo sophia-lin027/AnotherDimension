e554bf7d0d849bc1c20190e0bf341205b1498bac
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-25 20:35:18 -0400

    added 5d

--- Tester.java
+++ Tester.java
@@ -4,13 +4,16 @@ public class Tester {
   public static void main(String[] args){
     int[] B = {1, 3, 5};
     int[][] A = {{1, 0, 12, -1}, {7, -2, 2, 1}, {-5, -2, 2, -9}};
+    int[][] C = {{1, 2, 3, 4}, {2, 3, 4, 1}, {3, 4, 1, 2}};
+    int[][] E = {{2, 4, 2}, {2, 2, 2}};
     System.out.println(ArrayOps.sum(B));
     System.out.println(ArrayOps.largest(B));
     System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
     System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
     System.out.println(ArrayOps.sum(A));
     System.out.println(Arrays.toString(ArrayOps.sumCols(A)));
-    System.out.println(ArrayOps.isRowMagic(A));
-    System.out.println(ArrayOps.isColMagic(A));
+    System.out.println(ArrayOps.isRowMagic(C));
+    System.out.println(ArrayOps.isColMagic(C));
+    System.out.println(ArrayOps.isLocationMagic(E, 1, 1));
   }
 }
